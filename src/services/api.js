62fcc121c3368487d7a2a411b9119e78add8408d
Anandangx <anandangx25@gmail.com>
2026-04-27 23:30:44 +0530

    Update API URL + production config

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8084",
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true, // important for CORS + cookies
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -12,6 +14,7 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+// Handle auth errors globally
 API.interceptors.response.use(
   (res) => res,
   (err) => {
