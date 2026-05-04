ed8f1bc9a70665c7c0ccf6530af60ef9f31165c7
Anandangx <anandangx25@gmail.com>
2026-05-04 22:44:11 +0530

    Fix env variables

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://corex-backend.up.railway.app"
+      ? "https://corex-management.vercel.app"
       : "http://localhost:8080",
 });
 
