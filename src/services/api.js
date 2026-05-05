2e8a1a997f6c2129996028c962530a1999058748
Anandangx <anandangx25@gmail.com>
2026-05-05 22:42:23 +0530

    fix: complete corrected codebase

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // ✅ from .env
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-// Attach token (only for protected APIs)
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
